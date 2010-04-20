7f75f7e46c648c2e772c661ac90f3cd62219a646
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-04-20 04:33:54 +0000

    Decided to use old-school support system of direct messaging the board support as it seems to be the most effective and productive way of doing support. Very resource efficient along with saving lots of money.
    git-svn-id: https://burst-dev.com/svn/music/trunk@22 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- app.help.php
+++ app.help.php
@@ -2,6 +2,7 @@
 <?php include 'statics/campfire.txt'; ?>
 </div>
 
+
 <?php explanation('1. Watch the Intro Videos','Having trouble? These videos should help out a little. Each link will redirect you to a page on screenr.'); ?>
 <div style="margin-left: 20px; margin-right: 20px;">
 <a href="http://screenr.com/62g" target="_blank">Uploading a Song From Your Computer to the Application</a><br />
@@ -17,18 +18,6 @@
 <?php } ?>
 
 <?php explanation('3. What is new?','Something could have gone wrong, check out what the developers are doing!'); ?>
-<div style="padding: 10px; margin-left: 10px; margin-right: 10px; border: 1px solid #d4dae8;">
+<div style="padding: 10px; margin-left: 10px; margin-bottom: 10px; margin-right: 10px; border: 1px solid #d4dae8;">
 <?php include 'statics/twitter.txt'; ?>
 </div>
-
-<?php explanation('<fb:intl>3. Ask Others</fb:intl>','Most of the time, the community will be able to help!'); ?>
-<div style="margin-left: 10px; margin-right: 10px; border-left: 1px solid #cccccc; border-right: 1px solid #cccccc;">
-<fb:board xid="help" numtopics="3" candelete="<?php if ($user == "1340490250") { echo 'true'; } else { echo 'false'; } ?>" canmark="<?php if ($user == "1340490250") { echo 'true'; } else { echo 'false'; } ?>">
-<fb:title><fb:intl>Community Forum</fb:intl></fb:title></fb:board>
-</div>
-
-<?php if($error == NULL AND $action == NULL) { ?>
-	<?php explanation('<fb:intl>4. Contact the Developers</fb:intl>','<fb:intl>This is your last resort if you cannot get an answer. Use the comment box below to contact the developers, all data here can only be seen by you and the developers. It usually takes up to twenty-four hours for a reply.</fb:intl>'); ?>
-<?php } ?>
-
-<?php include 'inc.tickets.php'; ?>
