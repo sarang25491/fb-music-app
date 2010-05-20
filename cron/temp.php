078e66100baa096626319390a660b32cb9b6723f
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-05-20 01:50:13 +0000

    A much better way of handling the temp files.
    git-svn-id: https://burst-dev.com/svn/music/trunk@56 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- cron/temp.php
+++ cron/temp.php
@@ -3,66 +3,12 @@ $pre = 'skip_fbapi';
 include '../include/config.php';
 ?>
 
+<?php exec('find ' . $config['server']['internal_url'] . 'users/temp/ -mtime +0.42 -exec rm {} \;'); // will delete anything older than an hour ?>
+
 <?php
-/* 
-PART 1: CLEAR EXPIRED DATABASE ENTRIES
-This will first select all values inside the database
-that are not within the 15 minute time frame
-| Current Time | -- Files within 15 minute frame -- | Current Time minus 15 | -- EXPIRED FILES -- |
-Any file in the Expired Files frame will be deleted 
-from both the file system and the database
-*/
-
-$minutes_to_subtract = 60;
-$expiration_time = date('Y-m-d H:i:s', strtotime('-'.$minutes_to_subtract.' min'));
-/* Set CRON JOB to every 5 minutes to remove temporary files that have expired over 15 minutes */
-$data_to_delete = $db->Raw("SELECT `user`,`location` FROM `userdb_temporary` WHERE `time` < '$expiration_time'");
-
-foreach($data_to_delete as $delete_queue) {
-	unlink($delete_queue['location']);
-	$db->Raw("DELETE FROM `userdb_temporary` WHERE `user`='$delete_queue[user]'");
-}
-
-/*
-PART 2: CHECK FILE SYSTEM EXPIRATION
-This will list all files in directory
-Check if the file exists in the database
-If it doesn't exist, remove the file
-*/
-
-// Directory listing function.
-function dirList ($directory) 
-{
-
-    // create an array to hold directory list
-    $results = array();
-
-    // create a handler for the directory
-    $handler = opendir($directory);
-
-    // keep going until all files in directory have been read
-    while ($file = readdir($handler)) {
-
-        // if $file isn't this directory or its parent, 
-        // add it to the results array
-        if ($file != '.' && $file != '..')
-            $results[] = $file;
-    }
-
-    // tidy up: close the handler
-    closedir($handler);
-
-    // done!
-    return $results;
-
-}
-
-$files = dirList('' . $config['server']['internal_url'] . 'users/temp/');
-foreach ($files as $file) {
-	$link_to_check = '' . $config['fb']['internal_url'] . 'users/temp/' . $file . '';
-	$existor = $db->Raw("SELECT COUNT(*) FROM `userdb_temporary` WHERE `location`='$link_to_check'");
-
-	if ($existor[0]['COUNT(*)'] == 0)
-		unlink('' . $config['server']['internal_url'] . 'users/temp/' . $file . '');
+$currentTempDb = $db->Raw("SELECT `location` FROM `userdb_temporary`");
+foreach ($currentTempDb as $file) {
+	if (!file_exists($file['location']))
+		$db->Raw("DELETE FROM `userdb_temporary` WHERE `location`='$file[location]'");
 }
 ?>
