7d3c14aca941ce5d5861176a503360b823d3e776
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-05-21 05:22:28 +0000

    Prettier messages to the help pages, specifically to the chat room.
    git-svn-id: https://burst-dev.com/svn/music/trunk@68 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- cron/campfire.php
+++ cron/campfire.php
@@ -16,7 +16,7 @@ print_r($room_data);
 
 $file = fopen('../statics/campfire.txt','w');
 if ($room_data['room']['open-to-guests']['value'] == 'true') {
-	fwrite($file,'<?php success(\'Support Chat Room Open: <a href="http://burst.campfirenow.com/' . $room_data['room']['active-token-value']['value'] . '">http://burst.campfirenow.com/' . $room_data['room']['active-token-value']['value'] . '</a>\',\'Looks like there is someone available to help! Feel free to drop by, leave a message on the <a href="http://www.facebook.com/board.php?uid=2436915755">boards</a>, or message the developer <a href="http://www.facebook.com/?compose=1&id=1340490250&sk=messages">directly</a>.\'); ?>');
+	fwrite($file,'<?php success(\'Support Chat Room Open: <a href="http://burst.campfirenow.com/' . $room_data['room']['active-token-value']['value'] . '">http://burst.campfirenow.com/' . $room_data['room']['active-token-value']['value'] . '</a>\',\'Someone is available to help! Feel free to drop by, leave a message on the <a href="http://www.facebook.com/board.php?uid=2436915755">boards</a>, or message the developer <a href="http://www.facebook.com/?compose=1&id=1340490250&sk=messages">directly</a>.\'); ?>');
 } else {
 	fwrite($file,'<?php error(\'Support Chat Room Closed\',\'Sorry about that, but no one is around... Please leave a message on the <a href="http://www.facebook.com/board.php?uid=2436915755">boards</a> (recommended), or message the developer <a href="http://www.facebook.com/?compose=1&id=1340490250&sk=messages">directly</a>.\'); ?>');
 }
