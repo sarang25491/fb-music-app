8c5965f2268fb1cf2028163e1584b35652a43789
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-06-06 21:41:52 +0000

    Fixing some odd syntax error that appeared.
    git-svn-id: https://burst-dev.com/svn/music/trunk@74 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- app.youtube.php
+++ app.youtube.php
@@ -121,13 +121,11 @@ if(isset($_GET['search'])) {
 	$db->Raw("UPDATE `userdb_uploads` SET `xid`=`id` WHERE `id`='$id'");
 
 	include 'fb.profile.php';
-	<?php
+
 	if($_POST['wall'])
 		if(isset($_GET['fb_page_id'])) { redirect('' . $config['fb']['fburl'] . '?tab=index&publish&fb_page_id=' . $_GET['fb_page_id'] . ''); } else { redirect('' . $config['fb']['fburl'] . '?tab=index&publish'); }
 	else
 		if(isset($_GET['fb_page_id'])) { redirect('' . $config['fb']['fburl'] . '?tab=index&fb_page_id=' . $_GET['fb_page_id'] . ''); } else { redirect('' . $config['fb']['fburl'] . '?tab=index'); }
-	?>
-
  
 }
 ?>
