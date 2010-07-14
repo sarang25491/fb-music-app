61c3286e1137738441fc04da0b9050832c3c730c
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-07-14 22:45:14 +0000

    Playlist header changed to make more sense for new time users on Facebook.
    git-svn-id: https://burst-dev.com/svn/music/trunk@93 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- app.index-header.php
+++ app.index-header.php
@@ -1,6 +1,6 @@
 <div style="margin-bottom: -5px; padding: 5px; border-bottom: 1px solid #cccccc; background-color: #eceff5;">
 <?php if(isset($_GET['fb_page_id'])) { ?>
-	You are currently editing page, <b><fb:name uid="<?php echo $_GET[fb_page_id]; ?>" /></b>, change back to your <b><a href="<?php echo $config['fb']['fburl'] ?>">profile</a></b>?
+	You are currently editing <b><fb:name uid="<?php echo $_GET[fb_page_id]; ?>" />'s playlist</b>, change back to your <b><a href="<?php echo $config['fb']['fburl'] ?>">profile playlist</a></b>?
 <?php } else { ?>
 	<?php 
 	$pdata = $db->Raw("SELECT `fb_page_id` FROM `pages` WHERE `owner`='$user'");
@@ -15,11 +15,11 @@
 		$page_string = substr_replace($page_string,"",-2);
 		?>
 		
-		You are currently editing your <b>profile</b>, change to page <?php echo $page_string; ?>? <b><a href="http://www.facebook.com/add.php?api_key=<?php echo $config['fb']['key']; ?>&pages&_fb_q=1">Add more</a></b>?
+		You are currently editing your <b>profile playlist</b>, change playlist to <?php echo $page_string; ?>? <b><a href="http://www.facebook.com/add.php?api_key=<?php echo $config['fb']['key']; ?>&pages&_fb_q=1">Add more</a></b>?
 	<?php 
 	} else {
 	?>
-		You are currently editing your <b>profile</b>. No pages have been found, <b><a href="http://www.facebook.com/add.php?api_key=<?php echo $config['fb']['key']; ?>&pages&_fb_q=1">add a page</a></b>?
+		You are currently editing your <b>profile playlist</b>. No other playlists have been found, <b><a href="http://www.facebook.com/add.php?api_key=<?php echo $config['fb']['key']; ?>&pages&_fb_q=1">add a page</a> playlist</b>?
 	<?php } ?>
 <?php } ?>
 </div>
