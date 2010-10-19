243318ee94686b0ded2b8818a0b65a09edcbd3d5
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-10-19 18:17:21 +0000

    Update to the help pages.
    git-svn-id: https://burst-dev.com/svn/music/trunk@116 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- app.help.php
+++ app.help.php
@@ -1,13 +1,29 @@
-<div style="margin-bottom: -10px;">
-<?php include 'statics/campfire.txt'; ?>
-</div>
+<center>
+<div style="width: 100%; margin: 10px; background-color: #ffebe8; border-top: 1px solid #dd3c10; border-bottom: 1px solid #dd3c10;">
+<table border="0" cellspacing="0" cellpadding="0">
+<tr>
+   <td>
+      <div style="margin: 10px 10px 10px 10px; padding: 10px; font-size: 16px; border: 1px solid #e2c822; background-color: #fff9d7;">
+         <a href="http://www.facebook.com/board.php?uid=2436915755">View Discussion Board</a>
+      </div>
+   </td>
 
-<fb:dialog id="support">
+   <td>
+      <div style="margin: 10px 10px 10px 0; padding: 10px; font-size: 16px; border: 1px solid #e2c822; background-color: #fff9d7;">
+         <a clicktoshowdialog="support">Submit Support Ticket</a>
+      </div>
+   </td>
+</tr>
+</table>
+</div>
+</center>
+
+<fb:dialog id="support" width="500">
 	<fb:dialog-title>Before Contacting Support</fb:dialog-title>
 	<fb:dialog-content>
 		<?php success('Please read the following support guidelines...', ''); ?>
-		<div style="padding: 10px; margin: 0 10px 10px 10px; border: 1px solid #d4dae8">
-			<div style="margin-left: -20px;">
+		<div style="padding: 10px; margin: 0 10px 0 10px; border: 1px solid #d4dae8">
+			<div style="margin: -10px 0 0 -20px;">
             <ol>
 				   <li>All requests should be in English, we can't read anything else.</li>
 				   <li>Make sure that you explain your situation thoroughly; give us what you were trying to do and what exactly happened (error messages, abnormalities, etc).</li>
@@ -18,11 +34,10 @@
 			<b>Please note that if your support request does not follow the above, it will automatically be ignored due to the high number of requests.</b>
       </div>
 		
-		<?php success('When you have read the above...<br /><a href="http://www.facebook.com/?compose=1&id=1340490250&sk=messages">Click here to submit a support request</a>',''); ?>
+		<?php success('<center>When you have read the above...<br /><a href="http://www.facebook.com/?compose=1&id=1340490250&sk=messages">Click to Submit a Support Request</a></center>',''); ?>
 	</fb:dialog-content>
 	<fb:dialog-button type="button" value="Close" close_dialog=1 />
 </fb:dialog>
-
 
 <?php explanation('1. Watch the Intro Videos','Having trouble? These videos should help out a little. Each link will redirect you to a page on screenr.'); ?>
 <div style="margin-left: 20px; margin-right: 20px;">
