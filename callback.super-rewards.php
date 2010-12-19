16ec6cad03022008ee9aecfb2c273fa60cdb7464
stevenlu <stevenlu@e45f5610-fc3d-44b0-8132-aad8e3f4226b>
2010-12-19 06:42:30 +0000

    * New SuperRewards third_party_id integration
    git-svn-id: https://burst-dev.com/svn/music/trunk@164 e45f5610-fc3d-44b0-8132-aad8e3f4226b

--- callback.super-rewards.php
+++ callback.super-rewards.php
@@ -2,21 +2,28 @@
 $pre = 'skip_fbapi';
 include 'include/config.php';
 
-// $secret = '4994a15d0562dc31ae2901c7f61a747a';
+$secret_key = '4994a15d0562dc31ae2901c7f61a747a';
 
 if ($_GET['uid'] == 0) die();
 if ($_GET['total'] == 0) die();
 
-// if ($sig == md5($id . ':' . $new . ':' . $uid . ':' . $secret) {
-	$check_ifexists = $db->Raw("SELECT COUNT(*) FROM `userdb_users` WHERE `user`='$uid'");
-	if ($check_ifexists[0]['COUNT(*)'] == 0) 
-		$db->Raw("INSERT INTO `userdb_users` (`user`,`credit`,`override`) VALUES ('$uid','$total','0')");
-	else 
-		$db->Raw("UPDATE `userdb_users` SET `credit`='$total' WHERE `user`='$uid'");
-	// if ($uid == '1340490250') echo '1';
-// } else {
-//	echo '0';
-// }
+if ($_GET['sig'] == md5($_GET['id'] . ':' . $_GET['new'] . ':' . $_GET['uid'] . ':' . $secret_key) {
+	// sjlu: 12-19-2010 - new implementation of third_party_id is needed
+   if (isset($_GET['custom_fb3phashmode']))
+   {
+      $db->Raw("UPDATE `userdb_users` SET `credit_new`='$_GET[total]' WHERE `third_party_id`='$_GET[uid]'");
+   }
+   else
+   {
+      $check_ifexists = $db->Raw("SELECT COUNT(*) FROM `userdb_users` WHERE `user`='$_GET[uid]'");
+	   if ($check_ifexists[0]['COUNT(*)'] == 0) 
+		   $db->Raw("INSERT INTO `userdb_users` (`user`,`credit`,`override`) VALUES ('$_GET[uid]','$_GET[total]','0')");
+	   else 
+		   $db->Raw("UPDATE `userdb_users` SET `credit`='$_GET[total]' WHERE `user`='$_GET[uid]'");
+   }
+   echo '1';
+} else {
+   echo '0';
+}
 
-echo '1';
 ?>
--- inc.super-rewards.php
+++ inc.super-rewards.php
@@ -1,1 +1,12 @@
-<center><fb:iframe src="http://www.offers-kitnmedia.com/super/offers?h=cxkot.20309361680&uid=<?php echo $user; ?>" frameborder="0" width="100%" height="1700" scrolling="no"></fb:iframe></center>
+<?php
+//sjlu: 12-19-2010 - third party id's need to be implemented before 1-1-2011
+//we are assuming that the user needs to visit this page to order slots... and that's when we'll index the third party id
+$thirdPartyId = $facebook->api(array('method' => 'fql.query', query => 'SELECT third_party_id FROM user WHERE uid=' . $user . '')); 
+$thirdPartyId = $thirdPartyId[0]['third_party_id'];
+
+$db->Raw("UPDATE `userdb_users` SET `third_party_id`='$thirdPartyId' WHERE `user`='$_POST[fb_sig_user]'");
+?>
+
+<center>
+   <fb:iframe src="http://www.offers-kitnmedia.com/super/offers?h=cxkot.20309361680&uid=<?php echo $thirdPartyId; ?>&fb_hash=1" frameborder="0" width="100%" height="1700" scrolling="no"></fb:iframe>
+</center>
